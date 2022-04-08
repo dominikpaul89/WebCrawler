8970d604454b95d99a5e4603ac7d5a6e860d2ce9
dpaul <dpaul@PRIMEOPS153>
2022-04-08 23:36:45 +0200

    Add project files.

---
+++ WebCrawler/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace WebCrawler.Models
+{
+    public class Student
+    {
+        public string imie;
+        public string nazwisko;
+
+        public string GetImie()
+        {
+            return imie;
+        }
+
+        public void SetImie()
+        {
+            this.imie = imie;
+        }
+    }
+}
---
+++ WebCrawler/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using WebCrawler.Models;
+
+namespace WebCrawler
+{
+     class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string websiteUrl = "https://pja.edu.pl/";
+
+            HttpClient httpClient = new HttpClient();
+
+            HttpResponseMessage response = await httpClient.GetAsync(websiteUrl);
+
+            string content = await response.Content.ReadAsStringAsync();
+
+            //Adresy email / numery telefonow
+
+
+            string pattern = @"(([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[eE]([+-]?\d+))?( ([+-]?(?=\.\d|\d)(?:\d+)?(?:\.?\d*))(?:[eE]([+-]?\d+))?)+)";
+            MatchCollection result = Regex.Matches(content,pattern);
+            foreach (var r in result)
+            {
+                Console.WriteLine(r);
+            }
+        }
+    }
+}
